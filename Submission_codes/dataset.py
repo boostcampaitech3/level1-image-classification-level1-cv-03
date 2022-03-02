61390e727535aa6fe171481725d477fafaf9b654
“KimDaeYu” <dea0323@gmail.com>
2022-03-02 02:18:12 +0000

    #3 add CenterCropAugmentation

--- Submission_codes/dataset.py
+++ Submission_codes/dataset.py
@@ -64,6 +64,16 @@ class CustomAugmentation:
     def __call__(self, image):
         return self.transform(image)
 
+class CenterCropAugmentation:
+    def __init__(self, resize, mean, std, **args):
+        self.transform = transforms.Compose([
+            CenterCrop((320, 320)),
+            ToTensor(),
+            Normalize(mean=mean, std=std),
+        ])
+
+    def __call__(self, image):
+        return self.transform(image)
 
 class MaskLabels(int, Enum):
     MASK = 0
